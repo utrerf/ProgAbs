23316e936a64a35ab493f4ed37c73717a96ff088
Francisco Utrera <francisco.utrera@outlook.com>
2017-05-29 09:52:33 -0700

    Added Structure Example

---
+++ Prog Abs/Prog Abs/StructureExample.cpp
@@ -0,0 +1,43 @@
+//
+//  This program generates a list of the powers of
+//  two, up to an exponent limit entered by the user
+//
+//  Created by Francisco Utrera on 5/29/17.
+//  Copyright © 2017 Francisco Utrera. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+/* Function Prototypes */
+
+int raiseToPower(int n, int k);
+
+/* Main program */
+
+int main() {
+    int limit;
+    cout << "This program lists powers of two." << endl;
+    cout << "Enter exponent limit:";
+    cin >> limit;
+    for (int i = 0; i <= limit; i++) {
+        cout << "2 to the " << i << " = "
+        << raiseToPower(2,i) << endl;
+    }
+    return 0;
+}
+
+/*
+ * Function: raiseToPower
+ * Usage: int p = raiseToPower(n, k);
+ * ----------------------------------
+ * Returns the integer n raised to the kth power.
+ */
+
+int raiseToPower(int n, int k) {
+    int result = 1;
+    for (int i = 0; i < k; i++) {
+        result *= n;
+    }
+    return result;
+}
