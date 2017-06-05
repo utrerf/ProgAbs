c66041a3f4067acfc6d3d3c0a336311095f40376
Francisco Utrera <francisco.utrera@outlook.com>
2017-06-05 08:07:16 -0700

    Finished Chapter 1

--- Prog Abs/Prog Abs/P12.cpp
+++ Prog Abs/Prog Abs/P12.cpp
@@ -6,17 +6,19 @@
 //  Copyright © 2017 Francisco Utrera. All rights reserved.
 //
 
+
 #include <iostream>
 #include <math.h>
 using namespace std;
 
+/*
 
 // Prototype Functions
-double aprox_pi(double i);
+long double aprox_pi(long double i);
 
 // Main Program
 int main() {
-    double number;
+    long double number;
     cout << "This program return an aproximation of PI given a number of iterations." << endl;
     cout << "How many iterations would you like to complete? ";
     cin >> number;
@@ -25,17 +27,21 @@ int main() {
     return 0;
 }
 
-double aprox_pi(double i) {
-    double approx = 0.0;
-    double const RADIUS = 2.0;
-    double mid = (RADIUS / (2.0 * i));
-    double const BASE = RADIUS / i;
-    while (i > 0) {
-        //double height = sqrt(RADIUS * RADIUS - mid * mid);
-        //double area = height * BASE;
-        //approx += area;
-        i -= 1;
+long double aprox_pi(long double i) {
+    long double iter = i;
+    long double approx = 0.0;
+    const long double RADIUS = 2.0;
+    long double mid = (RADIUS / (2.0 * i));
+    long double const BASE = RADIUS / i;
+    while (i > 0.0) {
+        long double height = sqrt(RADIUS * RADIUS - mid * mid);
+        long double area = height * BASE;
+        approx += area;
+        mid += BASE;
+        i -= 1.0;
     }
-    cout << approx << endl;
+    cout << "PI approximates to " << approx << " given "<< iter << " iterations." << endl;
     return 0;
 }
+
+*/
