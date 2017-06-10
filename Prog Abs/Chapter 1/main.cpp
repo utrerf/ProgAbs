7f65a986470bd422744d25acbc8e3b1de2a76a48
Francisco Utrera <francisco.utrera@outlook.com>
2017-06-10 15:38:53 -0700

    new target

---
+++ Prog Abs/Chapter 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chapter 1
+//
+//  Created by Francisco Utrera on 6/10/17.
+//  Copyright © 2017 Francisco Utrera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
